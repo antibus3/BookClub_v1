4c40ada9c47798077516f48e2da1d5bde04ccebb
aKustov <antibus3@yandex.ru>
2020-11-08 16:27:46 +0500

    Рабочий semantic

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const Funnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
@@ -20,5 +21,21 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  let extraAssets = new Funnel('semantic/dist/themes', {
+    srcDir: '/',
+    include: ['**/*'],
+    destDir: '/assets/themes'
+  });
+
+  app.import({
+    development: 'vendor/semantic.js',
+    production:  'vendor/semantic.min.js'
+  });
+
+  app.import({
+    development: 'vendor/semantic.css',
+    production:  'vendor/semantic.min.css'
+  });
+
+  return app.toTree(extraAssets);
 };
