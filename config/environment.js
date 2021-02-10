7a78af5731b12bb027bc4836e5c9c34b10264862
aKustov <antibus3@yandex.ru>
2021-02-10 14:17:58 +0500

    4

--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.rootURL = '/REPOSITORY_NAME/';
+    ENV.rootURL = '/azurePipelines';
   }
 
   if (environment === 'test') {
@@ -42,12 +42,12 @@ module.exports = function(environment) {
 
     ENV.APP.rootElement = '#ember-testing';
     ENV.APP.autoboot = false;
-    ENV.rootURL = '/REPOSITORY_NAME/';
+    ENV.rootURL = '/azurePipelines';
   }
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.rootURL = '/REPOSITORY_NAME/';
+    ENV.rootURL = '/azurePipelines';
   }
 
   return ENV;
